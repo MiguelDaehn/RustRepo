f81b011d94066b8297350abb37dbe3402a9f265c
MiguelDaehn <migueldaehnribeiro@gmail.com>
2025-02-13 01:41:08 -0300

    learning

---
+++ main/target/debug/deps/main-479d1617bf5520fc.d
@@ -0,0 +1,5 @@
+/home/kanamori/RustroverProjects/RustRepo/main/target/debug/deps/main-479d1617bf5520fc: src/main.rs
+
+/home/kanamori/RustroverProjects/RustRepo/main/target/debug/deps/main-479d1617bf5520fc.d: src/main.rs
+
+src/main.rs:
---
+++ main/target/debug/main.d
@@ -0,0 +1,1 @@
+/home/kanamori/RustroverProjects/RustRepo/main/target/debug/main: /home/kanamori/RustroverProjects/RustRepo/main/src/main.rs
